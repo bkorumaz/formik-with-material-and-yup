434f974d911b8dc2b2a470df7615fa4d38dfe231
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-21 15:57:10 +0300

    turned form components into material ones

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TextField, MenuItem } from "@material-ui/core";
 import { Form, Formik, Field } from "formik";
 
 function App() {
@@ -6,21 +7,26 @@ function App() {
     name: "",
     surname: "",
     email: "",
-    phoneNumber: "",
-    address: "",
-    birthDate: new Date()
+    age: "",
+    userType: "",
   };
 
   return (
     <div>
+    <h2>Register</h2>
       <Formik initialValues={initialValues} onSubmit={() => {}}>
-        {({ values }) =>(
-        <Form>
-        <Field name="name"/>
-        <Field name="surname"/>
-        <Field name="email"/>
-        <pre>{JSON.stringify(values, null, 4)}</pre>
-        </Form>
+        {({ values }) => (
+          <Form>
+            <Field name="name" as={TextField} label="Name" />
+            <Field name="surname" as={TextField} label="Surname"/>
+            <Field name="email" as={TextField} label="Email" type="email"/>
+            <Field name="age" type="number" as={TextField} label="Age" />
+            <Field name="userType" as={TextField} select label="User Type">
+              <MenuItem value={0}>Administrator</MenuItem>
+              <MenuItem value={1}>Normal</MenuItem>
+            </Field>
+            <pre>{JSON.stringify(values, null, 4)}</pre>
+          </Form>
         )}
       </Formik>
     </div>
