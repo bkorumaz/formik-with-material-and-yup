1d406445a4f5af80446d07a34cde9d74a1939219
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-22 10:26:25 +0300

    latest changes in App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import { useForm } from "react-hook-form";
 function App() {
   const { register, handleSubmit, watch, errors } = useForm();
 
-  const onSubmit = data => { console.log(data) }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-    <TextField name="userName" label='Username' error={!!errors.userName} helperText={errors.userName && 'username required'} inputRef={register({ required:true,pattern: /^[A-Za-z]+$/i })}/>
-    <input name="firstName" ref={register({ required: true, maxLength: 20 })} />
-    <input name="lastName" ref={register({ pattern: /^[A-Za-z]+$/i })} />
-    <input name="age" type="number" ref={register({ min: 18, max: 99 })} />
-    <input type="submit" />
-  </form>
+      <TextField
+        name="userName"
+        label="Username"
+        error={!!errors.userName}
+        helperText={errors.userName && "username required"}
+        inputRef={register({ required: true, pattern: /^[A-Za-z]+$/i })}
+      />
+      <input
+        name="firstName"
+        ref={register({ required: true, maxLength: 20 })}
+      />
+      <input name="lastName" ref={register({ pattern: /^[A-Za-z]+$/i })} />
+      <input name="age" type="number" ref={register({ min: 18, max: 99 })} />
+      <input type="submit" />
+    </form>
   );
 }
 
